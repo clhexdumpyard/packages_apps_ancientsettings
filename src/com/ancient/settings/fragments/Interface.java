a2f5e87d02602939be880a34203b76d6907ce7f3
Alex <du.alexcruz@gmail.com>
2021-01-08 12:56:50 +0700

    AncientSettings: Add DU themes [3/3]
    SDB: Adapt for AOSP Settings
    
    Special Thanks to @imjyotiraditya for the help
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/ancient/settings/fragments/Interface.java
+++ src/com/ancient/settings/fragments/Interface.java
@@ -16,16 +16,46 @@
  */
 package com.ancient.settings.fragments;
 
+import android.app.Activity;
+import android.app.UiModeManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
+import androidx.preference.*;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import static android.os.UserHandle.USER_SYSTEM;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.OverlayCategoryPreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
+import android.provider.SearchIndexableResource;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
 
 import com.ancient.settings.display.AccentColorPreferenceController;
 import com.ancient.settings.display.QsTileStylePreferenceController;
@@ -33,18 +63,27 @@ import com.ancient.settings.display.QsClockStylePreferenceController;
 import com.ancient.settings.display.DgCornerStylePreferenceController;
 import com.ancient.settings.display.SbHeightStylePreferenceController;
 
+import com.android.internal.util.ancient.ThemesUtils;
+import com.android.internal.util.ancient.AncientUtils;
+
 import com.android.settings.R;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
-public class Interface extends DashboardFragment {
+public class Interface extends DashboardFragment implements
+        OnPreferenceChangeListener, Indexable {
+
     private static final String TAG = "Interface";
+    private static final String PREF_THEME_SWITCH = "theme_switch";
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.ANCIENT_SETTINGS;
-    }
+    private IOverlayManager mOverlayService;
+    private UiModeManager mUiModeManager;
+
+    private ListPreference mThemeSwitch;
 
     @Override
     protected String getLogTag() {
@@ -54,6 +93,18 @@ public class Interface extends DashboardFragment {
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.ancient_settings_interface;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mUiModeManager = getContext().getSystemService(UiModeManager.class);
+
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        setupThemeSwitchPref();
     }
 
     @Override
@@ -78,5 +129,63 @@ public class Interface extends DashboardFragment {
         controllers.add(new DgCornerStylePreferenceController(context));
         controllers.add(new SbHeightStylePreferenceController(context));
         return controllers;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mThemeSwitch) {
+            String theme_switch = (String) objValue;
+            final Context context = getContext();
+            switch (theme_switch) {
+                case "1":
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK);
+                    break;
+                case "2":
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
+                    break;
+                case "3":
+                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
+                    break;
+            }
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+            } catch (RemoteException ignored) {
+            }
+        }
+        return true;
+    }
+
+    private void setupThemeSwitchPref() {
+        mThemeSwitch = (ListPreference) findPreference(PREF_THEME_SWITCH);
+        mThemeSwitch.setOnPreferenceChangeListener(this);
+        if (AncientUtils.isThemeEnabled("com.android.theme.solarizeddark.system")) {
+            mThemeSwitch.setValue("3");
+        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
+            mThemeSwitch.setValue("2");
+        } else {
+            mThemeSwitch.setValue("1");
+        }
+    }
+
+    private void handleBackgrounds(Boolean state, Context context, int mode, String[] overlays) {
+        if (context != null) {
+            Objects.requireNonNull(context.getSystemService(UiModeManager.class))
+                    .setNightMode(mode);
+        }
+        for (int i = 0; i < overlays.length; i++) {
+            String background = overlays[i];
+            try {
+                mOverlayService.setEnabled(background, state, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ANCIENT_SETTINGS;
     }
 }
