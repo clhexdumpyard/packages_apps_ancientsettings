f60db31795a45c8648bb70325b3f60189e5ce6e9
Idc <59904122+IDontCare-05@users.noreply.github.com>
2021-11-26 03:18:09 +0700

    AncientSettings: AncientUI
    And add some features

--- src/com/ancient/settings/fragments/StatusBar.java
+++ src/com/ancient/settings/fragments/StatusBar.java
@@ -15,13 +15,19 @@
  */
 package com.ancient.settings.fragments;
 
+import static android.os.UserHandle.USER_CURRENT;
+
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.Context;
 import android.content.res.Resources;
+import android.content.om.IOverlayManager;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.provider.Settings;
 
 import androidx.preference.*;
@@ -32,6 +38,10 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.ancient.settings.preferences.SystemSettingSwitchPreference;
+
+import com.android.internal.util.ancient.AncientUtils;
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
@@ -47,10 +57,13 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String PREF_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String COMBINE_STATUSBAR_SIGNAL = "combine_statusbar_signal";
     private static final String KEY_STATUS_BAR_LOGO = "status_bar_logo";
-
+    private static final String CHARGING_BLEND_COLOR = "CHARGING_BLEND_COLOR";
+    private static final String FILL_BLEND_COLOR = "FILL_BLEND_COLOR";
+    private static final String CUSTOM_BLEND_COLOR = "CUSTOM_BLEND_COLOR";
+    
     private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
+    private static final int BATTERY_STYLE_TEXT = 19;
+    private static final int BATTERY_STYLE_HIDDEN = 20;
     private static final int BATTERY_PERCENT_HIDDEN = 0;
 
     private ListPreference mBatteryPercent;
@@ -58,6 +71,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private int mBatteryPercentValue;
     private SystemSettingSwitchPreference mCombineStatusbarSignal;
     private SwitchPreference mShowAncientLogo;
+    private ColorPickerPreference mblendCC;
+    private ColorPickerPreference mblendFC;
+    private SystemSettingSwitchPreference mblendSwitch;
+    
+    private IOverlayManager mOverlayService;  
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -65,6 +83,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.ancient_settings_statusbar);
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+        
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
 
         int batterystyle = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
@@ -93,6 +114,37 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mShowAncientLogo.setChecked((Settings.System.getInt(getContentResolver(),
         Settings.System.STATUS_BAR_LOGO, 0) == 1));
         mShowAncientLogo.setOnPreferenceChangeListener(this);
+        
+        mblendCC = (ColorPickerPreference) findPreference(CHARGING_BLEND_COLOR);
+        int blendCC = Settings.System.getInt(getContentResolver(),
+                "CHARGING_BLEND_COLOR", 0xFF00FF00);
+        mblendCC.setNewPreviewColor(blendCC);
+        mblendCC.setAlphaSliderEnabled(true);
+        String blendCCHex = String.format("#%08x", (0xFF00FF00 & blendCC));
+        if (blendCCHex.equals("#FF00FF00")) {
+            mblendCC.setSummary(R.string.color_default);
+        } else {
+            mblendCC.setSummary(blendCCHex);
+        }
+        mblendCC.setOnPreferenceChangeListener(this);
+        
+        mblendFC = (ColorPickerPreference) findPreference(FILL_BLEND_COLOR);
+        int blendFC = Settings.System.getInt(getContentResolver(),
+                "FILL_BLEND_COLOR", 0xFF00FF00);
+        mblendFC.setNewPreviewColor(blendFC);
+        mblendFC.setAlphaSliderEnabled(true);
+        String blendFCHex = String.format("#%08x", (0xFF00FF00 & blendFC));
+        if (blendFCHex.equals("#FF00FF00")) {
+            mblendFC.setSummary(R.string.color_default);
+        } else {
+            mblendFC.setSummary(blendCCHex);
+        }
+        mblendFC.setOnPreferenceChangeListener(this);  
+        
+        mblendSwitch = (SystemSettingSwitchPreference) findPreference(CUSTOM_BLEND_COLOR);
+        mblendSwitch.setChecked((Settings.System.getInt(getContentResolver(),
+        "CUSTOM_BLEND_COLOR", 0) == 1));
+        mblendSwitch.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -126,6 +178,38 @@ public class StatusBar extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_LOGO, value ? 1 : 0);
+            return true;
+        } else if (preference == mblendCC) {
+            String blendCC = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (blendCC.equals("#FF00FF00")) {
+                preference.setSummary(R.string.color_default);
+            } else {
+                preference.setSummary(blendCC);
+            }
+            int intblendCC = ColorPickerPreference.convertToColorInt(blendCC);
+            Settings.System.putInt(getContentResolver(),
+                    "CHARGING_BLEND_COLOR", intblendCC);
+            AncientUtils.showSystemUiRestartDialog(getContext());
+            return true;   
+        } else if (preference == mblendFC) {
+            String blendFC = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (blendFC.equals("#FF00FF00")) {
+                preference.setSummary(R.string.color_default);
+            } else {
+                preference.setSummary(blendFC);
+            }
+            int intblendFC = ColorPickerPreference.convertToColorInt(blendFC);
+            Settings.System.putInt(getContentResolver(),
+                    "FILL_BLEND_COLOR", intblendFC);
+            AncientUtils.showSystemUiRestartDialog(getContext());
+            return true; 
+        } else if  (preference == mblendSwitch) {
+            boolean valuecrot = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    "CUSTOM_BLEND_COLOR", valuecrot ? 1 : 0);
+            AncientUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;
