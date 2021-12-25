d6cd7ce3ea3c88137c5108e47526a51d603afbe8
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-25 16:04:27 +0700

    AncientSettings: Allow scheduling always on display [2/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/ancient/settings/fragments/LockScreen.java
+++ src/com/ancient/settings/fragments/LockScreen.java
@@ -49,8 +49,10 @@ public class LockScreen extends SettingsPreferenceFragment implements
 
     private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
     private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
 
     private FingerprintManager mFingerprintManager;
+    Preference mAODPref;
     private SwitchPreference mFingerprintSuccessVib;
     private SwitchPreference mFingerprintErrorVib;
 
@@ -82,6 +84,32 @@ public class LockScreen extends SettingsPreferenceFragment implements
         } else {
             prefSet.removePreference(mFingerprintSuccessVib);
             prefSet.removePreference(mFingerprintErrorVib);
+        }
+
+        mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAlwaysOnSummary();
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            case 0:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case 1:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case 2:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
         }
     }
 
