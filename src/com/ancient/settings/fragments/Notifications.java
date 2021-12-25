96ce49477d3f35256e01d1387666fc7e84cc1e61
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-25 15:55:45 +0700

    AncientSettings: Add DND & Rate settings to flash on call [3/3]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/ancient/settings/fragments/Notifications.java
+++ src/com/ancient/settings/fragments/Notifications.java
@@ -67,9 +67,11 @@ public class Notifications extends SettingsPreferenceFragment
     private static final String NOTIFICATION_MATERIAL_DISMISS_COLOR_BGSTYLE = "NOTIFICATION_MATERIAL_DISMISS_COLOR_BGSTYLE";
     private static final String STATUSBAR_ICONS_STYLE = "STATUSBAR_ICONS_STYLE";
     private static final String HEADER_ICONS_STYLE = "HEADER_ICONS_STYLE";
-    
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     private static final String KEY_EDGE_LIGHTNING = "pulse_ambient_light";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
 
     private Preference mChargingLeds;
     private ColorPickerPreference mCColorIc;
@@ -77,6 +79,9 @@ public class Notifications extends SettingsPreferenceFragment
     private SystemSettingSwitchPreference mSBColorIc;
     private SystemSettingSwitchPreference mHEADColorIc;
     private SystemSettingMasterSwitchPreference mEdgeLightning;
+    private CustomSeekBarPreference mFlashOnCallRate;
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,6 +95,24 @@ public class Notifications extends SettingsPreferenceFragment
         if (!AncientUtils.isVoiceCapable(getActivity())) {
                 prefScreen.removePreference(incallVibCategory);
         }
+
+        mFlashOnCallRate = (CustomSeekBarPreference)
+                findPreference(PREF_FLASH_ON_CALL_RATE);
+        int value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+        mFlashOnCallRate.setValue(value);
+        mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+        mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                findPreference(PREF_FLASH_ON_CALL_DND);
+        value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashOnCallIgnoreDND.setVisible(value > 1);
+        mFlashOnCallRate.setVisible(value != 0);
+
+        mFlashOnCall = (SystemSettingListPreference)
+                findPreference(PREF_FLASH_ON_CALL);
+        mFlashOnCall.setOnPreferenceChangeListener(this);
 
         mChargingLeds = (Preference) findPreference("charging_light");
         if (mChargingLeds != null
@@ -171,6 +194,18 @@ public class Notifications extends SettingsPreferenceFragment
         } else if (preference == mHEADColorIc) {
             AncientUtils.showSystemUiRestartDialog(getContext());
             return true; 
+        } else if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) objValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
         } else if (preference == mEdgeLightning) {
             boolean value = (Boolean) objValue;
             Settings.System.putIntForUser(resolver, KEY_EDGE_LIGHTNING,
