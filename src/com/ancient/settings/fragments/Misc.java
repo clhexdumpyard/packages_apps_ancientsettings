0cfba646c3c2e3d9d75cee4a4e71cc91e261a9ab
Idc <59904122+IDontCare-05@users.noreply.github.com>
2022-01-24 15:35:36 +0700

    Update Misc.java

--- src/com/ancient/settings/fragments/Misc.java
+++ src/com/ancient/settings/fragments/Misc.java
@@ -16,16 +16,24 @@
 package com.ancient.settings.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.provider.Settings;
-
 import androidx.preference.*;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
@@ -37,21 +45,98 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.ancient.settings.preferences.CustomSecureSeekBarPreference;
+import com.ancient.settings.preferences.SecureSettingSwitchPreference;
+
+import android.provider.SearchIndexableResource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
+
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class Misc extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Misc";
+    
+    private static final String SYSUI_ROUNDED_SIZE = "SYSUI_ROUNDED_SIZE";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "SYSUI_ROUNDED_CONTENT_PADDING";
+    private static final String SYSUI_ROUNDED_FWVALS = "SYSUI_ROUNDED_FWVALS";
+    
+    private CustomSecureSeekBarPreference mCornerRadius;
+    private CustomSecureSeekBarPreference mContentPadding;
+    private SecureSettingSwitchPreference mRoundedFwvals;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.ancient_settings_misc);
+        
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        
+        final ContentResolver resolver = getActivity().getContentResolver();
+        
+        Resources res = null;
+        Context ctx = getContext();
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        // Rounded Corner Radius
+        mCornerRadius = (CustomSecureSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
+        int resourceIdRadius = (int) ctx.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
+        int cornerRadius = Settings.Secure.getIntForUser(ctx.getContentResolver(), "SYSUI_ROUNDED_SIZE",
+                ((int) (resourceIdRadius / density)), UserHandle.USER_CURRENT);
+        mCornerRadius.setValue(cornerRadius);
+        mCornerRadius.setOnPreferenceChangeListener(this);
+
+        // Rounded use Framework Values
+        mRoundedFwvals = (SecureSettingSwitchPreference) findPreference(SYSUI_ROUNDED_FWVALS);
+        mRoundedFwvals.setOnPreferenceChangeListener(this);
+        
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        Resources res = null;
+        Context ctx = getContext();
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (preference == mCornerRadius) {
+            Settings.Secure.putIntForUser(getContext().getContentResolver(), "SYSUI_ROUNDED_SIZE",
+                    (int) newValue, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mRoundedFwvals) {
+            restoreCorners();
+            return true;
+        }
         return false;
+    }
+    
+    private void restoreCorners() {
+        Resources res = null;
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        Context ctx = getContext();
+
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int resourceIdRadius = (int) ctx.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
+        mCornerRadius.setValue((int) (resourceIdRadius / density));
     }
 
     @Override
@@ -63,6 +148,24 @@ public class Misc extends SettingsPreferenceFragment
      * For Search.
      */
 
-    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.ancient_settings_misc);
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.ancient_settings_misc;
+                    result.add(sir);
+                    return result;
+                }
+
+          @Override
+               public List<String> getNonIndexableKeys(Context  context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
